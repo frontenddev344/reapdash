465a2ef4e8a9f54c9cd00a0f866de1ba76841dc4
frontenddev <frontendev344@gmail.com>
2024-09-19 13:05:02 +0530

    css

---
+++ js/script.js
@@ -0,0 +1,52 @@
+
+    $(".mobile_btn").click(function(){
+  $(".links_bar").toggleClass("sidebar");
+});
+
+
+
+
+
+//  droupdown
+
+    $(document).ready(function(){
+  $(".links_bar li").click(function(){
+    $(".droupdown").toggle();
+  });
+});
+
+
+
+
+
+    $('.banner-slide').owlCarousel({
+    loop: true,
+    margin: 10,
+    slideTransition: 'linear', // Smooth linear transition
+    items: 1,
+    responsiveClass: true,
+    autoplay: true,
+    autoplayTimeout: 2000, // Time between slides
+    autoplaySpeed: 16000, // Smooth autoplay speed
+    nav: false,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 1,
+        },
+        1000: {
+            items: 1,
+        }
+    }
+});
+
+$('.banner-slide').on('changed.owl.carousel', function(event) {
+    // Pause autoplay for 20 seconds (20,000 ms) after each slide
+    $('.banner-slide').trigger('stop.owl.autoplay'); // Stop autoplay
+    setTimeout(function() {
+        $('.banner-slide').trigger('play.owl.autoplay', [2000]); // Resume autoplay after pause
+    }, 20000); // 20-second pause
+});
+
