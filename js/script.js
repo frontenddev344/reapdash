723d7f0401b47ab921457803be8a5f2cc15bed73
frontenddev <frontendev344@gmail.com>
2024-09-20 13:57:39 +0530

    css

--- js/script.js
+++ js/script.js
@@ -160,3 +160,23 @@ $(document).ready(function() {
         $carousel.find('.owl-item.active').eq(middleIndex).addClass('middle-slide');
     }
 });
+
+
+// galery
+
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+
+  $('.slider-nav').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode:false,
+    focusOnSelect: true
+  });
